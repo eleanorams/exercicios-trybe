178f2830b318802a9e63b0d3deb9c1206889986a
eleanora <eleanora.ms@gmail.com>
2022-11-26 16:46:57 -0300

    exercicio 5

--- fundamentos/secao-04-introducao-js/exercicio4.js
+++ fundamentos/secao-04-introducao-js/exercicio4.js
@@ -3,10 +3,10 @@ let novo = 0;
 
 // Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
 
-
 for (let index = 0; index < numbers.length; index++) {
-    novo += numbers[index];  
-} var resultado = novo / numbers.length;
+    novo += numbers[index];
+}
+var resultado = novo / numbers.length;
 if (resultado > 20) {
     console.log("valor maior que 20");
 } else {
