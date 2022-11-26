f80d90b4a99702791898a20b97342193e3201726
eleanora <eleanora.ms@gmail.com>
2022-11-26 17:29:58 -0300

    exercicio 7 e 8

---
+++ fundamentos/secao-04-introducao-js/exercicio8.js
@@ -0,0 +1,6 @@
+// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+let numeros = [];
+
+for (let index = 1; index < 26; index += 1) {
+    numeros.push(index)
+} console.log(numeros);
