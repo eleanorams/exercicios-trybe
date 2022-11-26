2081e75f7ec81c66710703ee98275c45b5cd6fb6
eleanora <eleanora.ms@gmail.com>
2022-11-26 16:15:53 -0300

    exercicio 2

---
+++ fundamentos/secao-04-introducao-js/exercicio2.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let novo = 0;
+
+
+// Some todos os valores contidos no array e imprima o resultado;
+
+// for (let index = 0; index < numbers.length; index++) {
+//     novo += numbers[index];  
+// } console.log(novo);
+
+for (let n of numbers) {
+    novo += n;
+} console.log(novo);
