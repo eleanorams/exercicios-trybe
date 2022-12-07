4f447f076ef02d61731077bda7ca17bccdd1fc14
eleanora <eleanora.ms@gmail.com>
2022-12-07 13:53:48 -0300

    exemplo scrpit

--- fundamentos/secao-04-introducao-js/dia-04/exercicio3.js
+++ fundamentos/secao-04-introducao-js/dia-04/exercicio3.js
@@ -1,9 +1,19 @@
 // Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
 
+// Exemplo de palíndromo: arara
 
-function reverseString(str) {
-    return str;
+// verificaPalindrome('arara')
+
+// Retorno esperado: true
+// verificaPalindrome('desenvolvimento')
+
+// Retorno esperado: false
+
+function palindromo(a) {
+    let split = a.split("");
+    let reverse = split.reverse();
+    let join = reverse.join("")
+    return join;
 }
-reverseString("hello");
 
-console.log(reverseString);
+console.log(palindromo('anel'));
