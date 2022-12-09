27f8e0180c6fc5b40349a27b664cfbf2248bbab6
eleanora <eleanora.ms@gmail.com>
2022-12-09 16:47:52 -0300

    exemplo 2 resolvido

--- fundamentos/secao-05-dom-seletores/dia-02/exemplo2/script2.js
+++ fundamentos/secao-05-dom-seletores/dia-02/exemplo2/script2.js
@@ -1,4 +1,29 @@
 // Crie um irmão para elementoOndeVoceEsta.
+// 1 passo: recuperar o elemento pai
+const pai = document.getElementById('pai');
+// 2 passo: criar um novo elemento
+const irmaoOndeVoceEsta = document.createElement('section');
+// 3 passo: atribuir um id para o elemento
+irmaoOndeVoceEsta.id = 'irmaoOndeVoceEsta';
+// 4 passo: appendChild
+pai.appendChild(irmaoOndeVoceEsta);
+
 // Crie um filho para elementoOndeVoceEsta.
+
+const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+
+const filhoOndeVoceEsta = document.createElement('section');
+
+filhoOndeVoceEsta.id = 'filhoOndeVoceEsta';
+
+elementoOndeVoceEsta.appendChild(filhoOndeVoceEsta);
+
 // Crie um filho para primeiroFilhoDoFilho.
-// A partir desse filho criado, acesse terceiroFilho.
+
+const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+
+const filhoDo1filho = document.createElement('section');
+
+filhoDo1filho.id = 'filhoDo1Filho';
+
+primeiroFilhoDoFilho.appendChild(filhoDo1filho);
