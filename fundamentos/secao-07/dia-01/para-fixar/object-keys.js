2bbc258182affd440186bac4e6d6396b36909b51
eleanora <eleanora.ms@gmail.com>
2023-01-18 21:51:15 -0300

    exericio 1

---
+++ fundamentos/secao-07/dia-01/para-fixar/object-keys.js
@@ -0,0 +1,27 @@
+const student1 = {
+    html: 'Muito Bom',
+    css: 'Bom',
+    javascript: 'Ótimo',
+    softskills: 'Ótimo',
+  };
+  
+  const student2 = {
+    html: 'Bom',
+    css: 'Ótimo',
+    javascript: 'Ruim',
+    softskills: 'Ótimo',
+    git: 'Bom', 
+  };
+  
+  const listSkills = (student) => {
+    const arrayOfSkills = Object.keys(student);
+    for (const index in arrayOfSkills) {
+      console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+    }
+  };
+  
+  console.log('Estudante 1');
+  listSkills(student1);
+  
+  console.log('Estudante 2');
+  listSkills(student2);
