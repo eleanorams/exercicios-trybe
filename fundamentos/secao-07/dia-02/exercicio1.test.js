cc7f3323e796bf513de63e80e9c96f3ee98014b4
eleanora <eleanora.ms@gmail.com>
2023-01-19 16:00:38 -0300

    exercicio 1

---
+++ fundamentos/secao-07/dia-02/exercicio1.test.js
@@ -0,0 +1,9 @@
+const myRemove = require('./exercicio1');
+
+describe('testa a função myRemove', () => {
+  it('blablabla', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
