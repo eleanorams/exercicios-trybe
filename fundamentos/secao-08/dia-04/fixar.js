bca507f3af41eabc3fa103b5668525a05e6f68b1
eleanora <eleanora.ms@gmail.com>
2023-01-30 10:56:29 -0300

    dia 04

---
+++ fundamentos/secao-08/dia-04/fixar.js
@@ -0,0 +1,57 @@
+// Faça uma função chamada fruitSalad, passando como parâmetro specialFruit e additionalItens; faça a função retornar uma lista única, contendo todos os itens da nossa salada de frutas, usando o spread.
+// Copiar
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Maçã', 'Banana', 'Morango'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['Uva', 'Manga', 'Abaxi'];
+
+const fruitSalad = (fruit, additional) => [...fruit, ...additional];
+
+console.log(fruitSalad(specialFruit, additionalItens));
+
+// Object Destructuring
+const character = {
+    name: 'Luke SkyWalker',
+    age: '53',
+    description: {
+      specieName: 'Human',
+      jedi: true,
+    },
+    homeWorld: {
+      name: 'Tatooine',
+      population: '200000',
+    },
+  };
+  
+  // desestruturando o objeto:
+  const { name, age, homeWorld: { name: planetName }, description: { jedi } } = character;
+  
+  // imprimindo os valores:
+  console.log(`Esse é o ${name}, ele tem ${age} anos, mora no planeta ${planetName} e, por incrível que possa parecer, ele ${jedi ? 'é um Jedi' : 'não é um Jedi'}.`);
+
+
+  const daysOfWeek = {
+    workDays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+    weekend: ['Saturday', 'Sunday'],
+  };
+  
+  const { workDays, weekend } = daysOfWeek;
+  console.log(workDays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+  console.log(weekend); // ['Saturday', 'Sunday']
+  
+  const weekdays = [...workDays, ...weekend];
+  console.log(weekdays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+  const product = {
+    name: 'Smart TV Crystal UHD',
+    price: '1899.05',
+    seller: 'Casas de Minas',
+  };
+  
+  const printProductDetails = ({ name, price, seller }) => {
+    console.log(`Promoção! ${name} por apenas ${price} é só aqui: ${seller}`);
+  };
+  
+  printProductDetails(product); // Promoção! Smart TV Crystal UHD por apenas 1899.05 é só aqui: Casas de Minas
+  
